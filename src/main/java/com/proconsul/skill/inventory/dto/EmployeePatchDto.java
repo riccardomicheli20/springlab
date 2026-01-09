5055b13b4a3f085e439fb0ffcb021e1689241762
Brblcu01 <luca.barba04@gmail.com>
2026-01-09 15:31:32 +0100

    EmployeePatchDto

---
+++ src/main/java/com/proconsul/skill/inventory/dto/EmployeePatchDto.java
@@ -0,0 +1,11 @@
+package com.proconsul.skill.inventory.dto;
+
+import com.proconsul.skill.inventory.entity.Skill;
+
+import java.sql.Date;
+import java.util.List;
+
+public class EmployeePatchDto {
+	
+
+}
